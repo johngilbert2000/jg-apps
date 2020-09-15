95f19ee90fa49f684579e9fc61487661fb4d2dc1
John Gilbert <johngilbert2571@gmail.com>
2020-09-15 14:59:37 +0800

    Small fixes

--- src/components/main/blocks/ContactBlock.tsx
+++ src/components/main/blocks/ContactBlock.tsx
@@ -42,6 +42,7 @@ const ContactBlock: React.FunctionComponent = () => {
           <i className="far fa-envelope"></i>
         </motion.p>
 
+        <input type="hidden" name="form-name" value="contact-form" />
         <motion.input required type="text" name="name" id="contact-name" placeholder="Su Nombre" initial={init} animate={final} transition={{...trans, delay: 0.7}} />
         <motion.input required type="email" name="email" id="contact-email" placeholder="Su Correo Electronico" initial={init} animate={final} transition={{...trans, delay: 0.8}} />
         <motion.textarea required name="contact-message" id="contact-message" placeholder="Mensaje" initial={init} animate={final} transition={{...trans, delay: 0.9}} ></motion.textarea>
@@ -56,6 +57,7 @@ const ContactBlock: React.FunctionComponent = () => {
           <i className="far fa-envelope"></i>
         </motion.p>
 
+        <input type="hidden" name="form-name" value="contact-form" />
         <motion.input required type="text" name="name" id="contact-name" placeholder="您的姓名" initial={init} animate={final} transition={{...trans, delay: 0.7}} />
         <motion.input required type="email" name="email" id="contact-email" placeholder="您的電子郵件" initial={init} animate={final} transition={{...trans, delay: 0.8}} />
         <motion.textarea required name="contact-message" id="contact-message" placeholder="訊息" initial={init} animate={final} transition={{...trans, delay: 0.9}} ></motion.textarea>
@@ -70,6 +72,7 @@ const ContactBlock: React.FunctionComponent = () => {
           <i className="far fa-envelope"></i>
         </motion.p>
 
+        <input type="hidden" name="form-name" value="contact-form" />
         <motion.input required type="text" name="name" id="contact-name" placeholder="您的姓名" initial={init} animate={final} transition={{...trans, delay: 0.7}} />
         <motion.input required type="email" name="email" id="contact-email" placeholder="您的电子邮件" initial={init} animate={final} transition={{...trans, delay: 0.8}} />
         <motion.textarea required name="contact-message" id="contact-message" placeholder="讯息" initial={init} animate={final} transition={{...trans, delay: 0.9}} ></motion.textarea>
